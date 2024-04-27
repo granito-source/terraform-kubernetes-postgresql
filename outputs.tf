a1370c1cab5acf5a8c0f23819e695692b2eab0c7
Alexei Yashkov <alexei.yashkov@yashkov.org>
2024-04-27 14:09:29 -0400

    Adjust the name of the default database account

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@ output "postgres_password" {
     depends_on  = [helm_release.postgresql]
     value       = random_password.postgres.result
     sensitive   = true
-    description = "postgresql password"
+    description = "postgres password"
 }
